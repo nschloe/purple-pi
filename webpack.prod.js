300f90e2c9b08933eacd0d7a611f9c92cd31dc93
Nico Schlömer <nico.schloemer@gmail.com>
2020-04-06 13:00:30 +0200

    avoid eval() in production builds

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,9 @@ const common = require("./webpack.common.js");
 module.exports = merge(common, {
   // Don't use "production"; this minifies the code and makes it less reviewable by
   // extension site maintainers.
-  // mode: "production"
-  mode: "development"
+  // mode: "production",
+  mode: "development",
+  // Use inline-source-map to avoid illegal eval()s in the generated code, cf.
+  // <https://github.com/webpack/webpack/issues/4899#issuecomment-609724897>.
+  devtool: "inline-source-map"
 });
