cc4b0086c75fcdebf164c10bc089cb58b467647a
Nico Schlömer <nico.schloemer@gmail.com>
2020-04-07 19:22:54 +0200

    add note about firefox

--- src/mathjax.js
+++ src/mathjax.js
@@ -3,4 +3,7 @@ MathJax = {
     fontURL: chrome.runtime.getURL("fonts"),
   },
 };
+// Note that, for Firefox, in the file https://github.com/mathjax/MathJax/issues/2399 the line
+// `import '../ui/menu/menu.js';` must be commented out,
+// cf. <https://github.com/mathjax/MathJax/issues/2399>.
 require("mathjax-full/components/src/tex-chtml/tex-chtml.js");
