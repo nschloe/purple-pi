f1998bc45718c759c7918a3874aba240a94e76d8
Nico Schlömer <nico.schloemer@gmail.com>
2020-04-07 16:24:39 +0200

    remove last traces of polyfill

--- src/background.js
+++ src/background.js
@@ -1,5 +1,3 @@
-var browser = require("webextension-polyfill");
-
 // https://stackoverflow.com/a/8498668/353337
 function url_domain(data) {
   var a = document.createElement("a");
@@ -8,86 +6,94 @@ function url_domain(data) {
 }
 
 // initialize default whitelist (if it doesn't have a value yet)
-browser.runtime.onInstalled.addListener(() => {
+chrome.runtime.onInstalled.addListener(() => {
   const defaultWhitelist = ["github.com", "gitlab.com"];
-  browser.storage.sync.get({ domainWhitelist: undefined }).then((result) => {
+  chrome.storage.sync.get({ domainWhitelist: undefined }, (result) => {
     // in chrome it's `undefined`, in firefox `null`
     if (
       result.domainWhitelist === undefined ||
       result.domainWhitelist === null
     ) {
-      browser.storage.sync.set({ ["domainWhitelist"]: defaultWhitelist });
+      chrome.storage.sync.set({ ["domainWhitelist"]: defaultWhitelist });
     }
   });
 });
 
 function isWhitelisted(url, callback) {
-  browser.storage.sync.get("domainWhitelist").then((result) => {
+  chrome.storage.sync.get("domainWhitelist", (result) => {
     callback(result.domainWhitelist.includes(url_domain(url)));
   });
 }
 
-function handleError(error) {
-  console.log(`Error: ${error}`);
-}
-
 function handleCheck(request, sender, sendResponse) {
-  var querying = browser.tabs.query({
-    active: true,
-    currentWindow: true,
-  });
-  querying.then((tabs) => {
-    isWhitelisted(tabs[0].url, (response) => {
-      sendResponse({ isWhitelisted: response });
-    });
-  }, handleError);
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true,
+    },
+    (tabs) => {
+      isWhitelisted(tabs[0].url, (response) => {
+        sendResponse({ isWhitelisted: response });
+      });
+    }
+  );
 }
 
 function handleInject(request, sender, sendResponse) {
   isWhitelisted(request.url, (response) => {
-    if (response) {
-      browser.tabs.executeScript({
-        file: "mathjax.js",
-      });
-    }
     // Send an empty response to avoid warning
-    sendResponse({});
+    if (response) {
+      chrome.tabs.executeScript(
+        {
+          file: "mathjax.js",
+        },
+        () => {
+          sendResponse({});
+        }
+      );
+    } else {
+      sendResponse({});
+    }
   });
 }
 
 function handleToggle(request, sender, sendResponse) {
-  var querying = browser.tabs.query({
-    active: true,
-    currentWindow: true,
-  });
-  querying.then((tabs) => {
-    // extract domain
-    const domain = url_domain(tabs[0].url);
-    // toggle domain from whitelist
-    browser.storage.sync.get("domainWhitelist").then((result) => {
-      if (result.domainWhitelist.includes(domain)) {
-        const index = result.domainWhitelist.indexOf(domain);
-        if (index > -1) {
-          result.domainWhitelist.splice(index, 1);
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true,
+    },
+    (tabs) => {
+      // extract domain
+      const domain = url_domain(tabs[0].url);
+      // toggle domain from whitelist
+      chrome.storage.sync.get("domainWhitelist", (result) => {
+        if (result.domainWhitelist.includes(domain)) {
+          const index = result.domainWhitelist.indexOf(domain);
+          if (index > -1) {
+            result.domainWhitelist.splice(index, 1);
+          }
+          chrome.storage.sync.set(
+            { domainWhitelist: result.domainWhitelist },
+            () => {
+              sendResponse({ isWhitelisted: false });
+            }
+          );
+        } else {
+          result.domainWhitelist.push(domain);
+          chrome.storage.sync.set(
+            { domainWhitelist: result.domainWhitelist },
+            () => {
+              sendResponse({ isWhitelisted: true });
+            }
+          );
         }
-        browser.storage.sync
-          .set({ domainWhitelist: result.domainWhitelist })
-          .then(() => {
-            sendResponse({ isWhitelisted: false });
-          });
-      } else {
-        result.domainWhitelist.push(domain);
-        browser.storage.sync
-          .set({ domainWhitelist: result.domainWhitelist })
-          .then(() => {
-            sendResponse({ isWhitelisted: true });
-          });
-      }
-    });
-  }, handleError);
+      });
+    }
+  );
 }
 
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type === "checkIfWhitelisted") {
     handleCheck(request, sender, sendResponse);
   } else if (request.type === "inject") {
