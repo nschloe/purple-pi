f230f188b031198f9351da0ac5f3104cc01dc123
Nico Schlömer <nico.schloemer@gmail.com>
2020-05-05 16:59:46 +0200

    don't need storate permissions

--- src/mathjax.js
+++ src/mathjax.js
@@ -2,5 +2,10 @@ window.MathJax = {
   chtml: {
     fontURL: chrome.runtime.getURL("fonts"),
   },
+  tex: {
+    inlineMath: [
+      ["$", "$"]
+    ]
+  }
 };
 require("mathjax-full/components/src/tex-chtml/tex-chtml.js");
