dd86de3e1cca415acf9a179d858717f10a6efd45
Nico Schlömer <nico.schloemer@gmail.com>
2021-05-11 18:04:04 +0200

    remove options, readme

--- src/content_script.js
+++ src/content_script.js
@@ -15,35 +15,8 @@ function stringToBoolean(string) {
   }
 }
 
-// input options
-const booleanOptionsTex = [
-  "processEscapes",
-  "processEnvironments",
-  "processRefs",
-  "useLabelIds",
-];
-const stringOptionsTex = [
-  "inlineMath",
-  "tags",
-  "tagSide",
-  "tagIndent",
-  "multlineWidth",
-];
-const numberOptionsTex = ["maxMacros", "maxBuffer"];
-// output options
-const booleanOptionsChtml = [
-  "matchFontHeight",
-  "mtextInheritFont",
-  "merrorInheritFont",
-  "mathmlSpacing",
-  "adaptiveCSS",
-];
-const stringOptionsChtml = ["displayAlign", "displayIndent"];
-const numberOptionsChtml = ["scale", "minScale", "exFactor"];
-
 const getInject = () => {
   var activate = false;
-  var params = {};
   for (link of document.getElementsByTagName("a")) {
     var url;
     try {
@@ -56,30 +29,7 @@ const getInject = () => {
       url.hostname == "github.com" &&
       url.pathname == "/nschloe/purple-pi"
     ) {
-      // https://docs.mathjax.org/en/latest/options/input/tex.html
-      // collect Boolean options
-      booleanOptionsTex.concat(booleanOptionsChtml).forEach((item) => {
-        if (url.searchParams.has(item)) {
-          params[item] = stringToBoolean(url.searchParams.get(item));
-        }
-      });
-      // collect string options
-      stringOptionsTex.concat(stringOptionsChtml).forEach((item) => {
-        if (url.searchParams.has(item)) {
-          const value = url.searchParams.get(item);
-          if (item === "inlineMath") {
-            params[item] = [[value, value]];
-          } else {
-            params[item] = value;
-          }
-        }
-      });
-      numberOptionsTex.concat(numberOptionsChtml).forEach((item) => {
-        if (url.searchParams.has(item)) {
-          params[item] = Number(url.searchParams.get(item));
-        }
-      });
-      // collect activation
+      // collect activation switch
       if (url.searchParams.has("activate")) {
         activate = true;
         break;
@@ -99,7 +49,6 @@ const renderMath = () => {
       { left: "\\(", right: "\\)", display: false },
       { left: "\\[", right: "\\]", display: true },
     ],
-    // • rendering keys, e.g.:
     throwOnError: false,
   });
 };
@@ -110,31 +59,4 @@ chrome.runtime.onMessage.addListener((message, _, sendResponse) => {
   } else if (message === "render-math") {
     renderMath();
   }
-
-  // // mathjax config
-  // window.MathJax = {
-  //   tex: {},
-  //   chtml: {
-  //     fontURL: chrome.runtime.getURL("fonts"),
-  //   },
-  // };
-  // // set options
-  // booleanOptionsTex
-  //   .concat(stringOptionsTex)
-  //   .concat(numberOptionsTex)
-  //   .forEach((item) => {
-  //     if (params.hasOwnProperty(item)) {
-  //       window.MathJax.tex[item] = params[item];
-  //     }
-  //   });
-  // booleanOptionsChtml
-  //   .concat(stringOptionsChtml)
-  //   .concat(numberOptionsChtml)
-  //   .forEach((item) => {
-  //     if (params.hasOwnProperty(item)) {
-  //       window.MathJax.chtml[item] = params[item];
-  //     }
-  //   });
-  // chrome.runtime.sendMessage({ params: params }, (response) => {});
-  // sendResponse({ inject: true });
 });
